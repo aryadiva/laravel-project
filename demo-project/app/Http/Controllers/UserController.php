8123590a9e8fcedf5d91aaec7b910159c3dcdc80
AryaDiv4 <98592190+aryadiva@users.noreply.github.com>
2024-11-30 22:03:08 +0700

    get & show data from db

---
+++ demo-project/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function getUser($id){
+        $user = User::find($id);
+        return view('profile',['user'=>$user]);
+    }
+}
--- demo-project/routes/web.php
+++ demo-project/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/user', function(){
+//     return view ('user');
+// });
+
+Route::get('/user1/{id}', function($id){
+    return "Something $id";
+});
+
+Route::get('/user/{id}', [UserController::class, 'getUser']);
